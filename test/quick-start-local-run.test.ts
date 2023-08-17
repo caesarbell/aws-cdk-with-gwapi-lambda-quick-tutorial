6bb389a7fedd8ef5e4dc5b640603ce3e58058d89
Caesar Bell <caesar.jd.bell@gmail.com>
2023-08-17 10:09:23 -0400

    Initial commit

---
+++ test/quick-start-local-run.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QuickStartLocalRun from '../lib/quick-start-local-run-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/quick-start-local-run-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QuickStartLocalRun.QuickStartLocalRunStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
